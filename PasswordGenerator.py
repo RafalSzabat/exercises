859f80c24f588897b4451cf78821aecf368329a6
Rafal Szabat <rafalszabat2@gmail.com>
2024-09-15 20:42:27 +0200

    Update PasswordGenerator.py
    Improved the code efficiency, deleted unnecessary lists and unnecessary addition of them. Code looks a lot better now.

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -7,31 +7,30 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '*', '+']
 
-#Welcome message and input from user
+#Welcome message and inputs from user
 
 print("Welcome to the NewPassword Generator!")
+
 nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#for loops adding random elements to lists
+#for loops adding random elements to the list
 
-random_letter = []
+random_password = []
 for letter in range(0, nr_letters):
-    random_letter += random.choice(letters)
-random_symbol = []
+    random_password += random.choice(letters)
 for symbol in range(0, nr_symbols):
-    random_symbol += random.choice(symbols)
-random_number = []
+    random_password += random.choice(symbols)
 for number in range(0, nr_numbers):
-    random_number += random.choice(numbers)
+    random_password += random.choice(numbers)
 
-#Adding the lists, shuffling them, and joining to create a password
+#Shuffling the list, formatting the list to a string
 
-password = random_letter + random_symbol + random_number
-random.shuffle(password)
-randomised_password = (''.join(password))
+random.shuffle(random_password)
 
-#printing the result
+randomised_password = (''.join(random_password))
+
+#Printing the result
 
 print(f'Your password is: {randomised_password}')
